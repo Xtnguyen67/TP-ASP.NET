f054bf0fb8b18fd12fc589073b24b96037f4af11
Xtnguyen67 <xtnguyen@outlook.fr>
2025-04-23 16:35:24 +0200

    Add files via upload

---
+++ Controllers/Avis.cs
@@ -0,0 +1,18 @@
+﻿namespace TPLOCAL1.Controllers
+{
+    public class Avis
+    {
+        public string? Nom { get; set; }
+        public string? Prenom { get; set; }
+        public string? Sexe { get; set; }
+        public string? Adresse { get; set; }
+        public int CodePostal { get; set; }
+        public string? Ville { get; set; }
+        public string? Email { get; set; }
+        public DateOnly DateDebFormation { get; set; }
+        public string? FSuivi { get; set; }
+        public string? FormationCobol { get; set; }
+        public string? FormationC { get; set; }
+        
+    }
+}
